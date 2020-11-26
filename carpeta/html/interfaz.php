ee80938ec7303462577b78f6baae00c7abdfa861
CJTC99 <cit2_18@alu.uabcs.mx>
2020-11-25 22:03:58 -0700

    subiendo nuevo archivo

---
+++ carpeta/html/interfaz.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Ghiblix
+	</title>
+</head>
+
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../css/intefaz.css?v=0.0.22" media="all">
+
+<body>
+
+
+
+<!--Etiqueta en donde se enceuntra el parallax, tiulo e imagen -->
+	<div class="hola2">
+
+		<img src="../img/logoo.png">
+
+		<h2>
+			Todas tus películas de studio ghibli en un solo sitio <br><br>
+
+			<span>
+				GHIBLIX
+			</span>
+		</h2>
+	</div>
+
+
+
+<!-- Etiqueta en donde se encuentra el slider de las peliculas top-->
+	<div class="hola3">
+		<h2>
+			Peliculas <span> Top </span>
+		</h2>
+		<!-- Cacda imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
+		<div class="slider">
+			<ul>
+				<li>
+					<a href="">
+						<img src="../img/castillo.png">
+					</a>
+					
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/mononoke.jpg">
+					</a>
+					
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/setsuko.jpg">
+					</a>
+					
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/marnie.jpg">
+					</a>
+					
+				</li>
+			</ul>
+		</div>
+
+	</div>
+
+
+
+	<div class="hola4">
+		<h2>
+			<span> Peliculas </span> recomendadas
+		</h2>
+		<!-- Cacda imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
+		<div class="peli">
+			<ul>
+			<li>
+
+				<a href="">
+					<img src="../img/1.jpg">
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="">
+					<img src="../img/2.jpg">
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="">
+					<img src="../img/3.jpg">
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="">
+					<img src="../img/4.jpg">
+				</a>
+
+			</li>
+
+		</ul>
+			
+		</div>
+
+		<!-- Cacda imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
+		<div class="peli2">
+
+			<ul>
+			<li>
+
+				<a href="">
+					<img src="../img/vecino.jpg">
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="">
+					<img src="../img/zen.jpg">
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="">
+					<img src="../img/himesama.jpg">
+				</a>
+
+			</li>
+
+			<li>
+
+				<a href="">
+					<img src="../img/kara.jpg">
+				</a>
+
+			</li>
+			
+		</ul>
+			
+		</div>
+		
+	</div>
+
+
+
+	<div class="fondo5">
+
+		<div class="hola5">
+
+	 	</div>
+
+	 	<div class="frase">
+	 		<h2>
+	 			La inspiración desbloquea el futuro  <br><br>
+	 			<span>
+	 				<i> - Hayao Miyazaki - </i>
+	 			</span>
+	 		</h2>
+	 	</div>
+	</div>
+
+	<div class="hola6">
+		<h2>
+			Todas  las <span> categorías </span>
+		</h2>
+
+		<div class="cate1">
+			<h2>
+				Aventuras
+			</h2>
+			<ul>
+				<li>
+					<a href="">
+						<img src="../img/ambulante.jpg">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/himesama.jpg">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/2.jpg">
+					</a>
+				</li>
+		
+			</ul>
+		</div>
+
+		<div class="cate2">
+			<h2>
+				Fantasía
+			</h2>
+			<ul>
+				<li>
+					<a href="">
+						<img src="../img/3.jpg">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/vecino.jpg">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/zen.jpg">
+					</a>
+				</li>
+		
+			</ul>
+		</div>
+
+		<div class="cate3">
+			<h2>
+				Recuentos de la vida
+			</h2>
+			<ul>
+				<li>
+					<a href="">
+						<img src="../img/tumba.jpg">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/kara.jpg">
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="../img/1.jpg">
+					</a>
+				</li>
+		
+			</ul>
+		</div>
+
+		
+	</div>
+
+	<div class="hola7">
+
+	</div>
+
+	<div class="hola8">
+		
+		<div class="fondo8">
+			<img src="../img/kawaii.jpeg">
+		</div>
+
+	</div>
+
+	<div class="hola9">
+		
+	</div>
+
+
+
+
+
+	<!-- Menu de los enlaces (sidebar horizontal fijo) -->
+	<div class="enlaces">
+
+
+		<ul>
+			<li>
+				<a href="">
+
+					Inicio
+
+				</a>
+			</li>
+			<li>
+				<a href="">
+
+					Películas
+
+				</a>
+			</li>
+			<li>
+				<a href="">
+
+					Mirar enlinea
+
+				</a>
+			</li>
+		</ul>
+
+	</div>
+
+	
+</body>
+</html>
