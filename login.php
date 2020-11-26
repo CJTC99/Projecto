29fde9b3d1265518ed1df33e3cf87a73d65a38b5
Alex MB <alexis3_18@alu.uabcs.mx>
2020-11-25 21:36:32 -0700

    login

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Iniciar sesión
+	</title>
+</head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="./carpeta/css/login.css?v=0.0.10" media="all">
+<body>
+
+
+<div class="fondo">
+	<!-- Fondo del Login-->
+	<div class="gi">
+		<img src="./carpeta/img/fondo1.jpg">
+	</div>
+	<!-- Parte donde se ingresaran los datos-->
+	<div class="tarjeta">
+
+		<div class="formulario">
+
+			<form action="">
+
+			<img src="./carpeta/img/totoro2.png">
+  			<input type="email" id="email" name="email" placeholder="Email" required="">
+  			<br><br>
+  			<img src="./carpeta/img/contrasena.png">
+  			<input type="password" id="email" name="email" placeholder="Contraseña" required="">
+  			<br>
+  			<a href="">¿Olvidaste tu contraseña?</a>
+  			<br>
+  			<input type="submit" value="Iniciar sesión" id="submit">
+  			
+
+		</form>
+		</div>
+		
+	</div>
+
+	<!--Foto de inicio -->
+	<div class="fotoTo">
+		<img src="./carpeta/img/totoro.png">
+		
+	</div>
+</div>
+
+
+<div class="footer">
+
+	<hr>
+
+	<p>
+		Integrantes:<br>
+		Martínez Barrios, Alexis <br>
+		Turrubiates Carrillo, Cinthya Josselyn
+	</p>
+
+	<label>
+		© Matínez & Turrubiates |  <span> All rights reserved 2020 </span>
+	</label>
+</div>
+
+
+
+</body>
+</html>
