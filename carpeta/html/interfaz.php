066dd7b6e1f1ac858703afd19ced501941b08560
Alex MB <alexis3_18@alu.uabcs.mx>
2020-12-04 04:28:27 -0700

    projecto v4

--- carpeta/html/interfaz.php
+++ carpeta/html/interfaz.php
@@ -7,16 +7,7 @@
 </head>
 
 <meta charset="utf-8">
-<<<<<<< HEAD
 <link rel="stylesheet" type="text/css" href="../css/intefaz.css?v=0.0.30" media="all">
-=======
-<<<<<<< HEAD
-<link rel="stylesheet" type="text/css" href="../css/intefaz.css?v=0.0.30" media="all">
-=======
-<link rel="stylesheet" type="text/css" href="../css/intefaz.css?v=0.0.22" media="all">
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
-
 <body>
 
 
@@ -38,15 +29,8 @@
 
 
 <!-- Etiqueta en donde se encuentra el slider de las peliculas top-->
-<<<<<<< HEAD
+
 	<div class="hola3" id="mirara">
-=======
-<<<<<<< HEAD
-	<div class="hola3" id="mirara">
-=======
-	<div class="hola3">
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 		<h2>
 			Peliculas <span> Top </span>
 		</h2>
@@ -84,18 +68,9 @@
 
 
 
-<<<<<<< HEAD
+
 	<div class="hola4" id="pelii">
 		<h2 >
-=======
-<<<<<<< HEAD
-	<div class="hola4" id="pelii">
-		<h2 >
-=======
-	<div class="hola4">
-		<h2>
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 			<span> Peliculas </span> recomendadas
 		</h2>
 		<!-- Cacda imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
@@ -105,14 +80,7 @@
 
 				<a href="">
 					<img src="../img/1.jpg">
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 				</a>
 
 			</li>
@@ -145,15 +113,7 @@
 			
 		</div>
 
-<<<<<<< HEAD
-		<!-- Cada imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
-=======
-<<<<<<< HEAD
-		<!-- Cada imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
-=======
-		<!-- Cacda imagen va a contener su efecto hover con el nombre de la pelicula y simbolo de repoducir -->
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
+
 		<div class="peli2">
 
 			<ul>
@@ -215,27 +175,11 @@
 
 	<div class="hola6">
 		<h2>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 			Todas  las <span> películas </span>
 		</h2>
 
 		<div class="cate1">
 
-<<<<<<< HEAD
-=======
-=======
-			Todas  las <span> categorías </span>
-		</h2>
-
-		<div class="cate1">
-			<h2>
-				Aventuras
-			</h2>
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 			<ul>
 				<li>
 					<a href="">
@@ -257,17 +201,6 @@
 		</div>
 
 		<div class="cate2">
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
-			<h2>
-				Fantasía
-			</h2>
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 			<ul>
 				<li>
 					<a href="">
@@ -289,17 +222,6 @@
 		</div>
 
 		<div class="cate3">
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
-			<h2>
-				Recuentos de la vida
-			</h2>
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 			<ul>
 				<li>
 					<a href="">
@@ -333,10 +255,7 @@
 			<img src="../img/kawaii.jpeg">
 		</div>
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
+
 		<div class="fondo9">
 
 			<a href="">
@@ -365,29 +284,15 @@
 
 	<label>
 		© Matínez & Turrubiates |  <span> All rights reserved 2020 </span>
-	</label>
-		
+	</label>	
 </div>
-<<<<<<< HEAD
-=======
-=======
-	</div>
 
-	<div class="hola9">
-		
-	</div>
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 
 
 
 
 
 	<!-- Menu de los enlaces (sidebar horizontal fijo) -->
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 	
 	<div class="enlaces">
 
@@ -396,47 +301,24 @@
 		<ul>
 			<li>
 				<a href="#">
-<<<<<<< HEAD
-=======
-=======
-	<div class="enlaces">
 
-
-		<ul>
-			<li>
-				<a href="">
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 
 					Inicio
 
 				</a>
 			</li>
 			<li>
-<<<<<<< HEAD
 				<a href="#pelii">
-=======
-<<<<<<< HEAD
-				<a href="#pelii">
-=======
-				<a href="">
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
+
 
 					Películas
 
 				</a>
 			</li>
 			<li>
-<<<<<<< HEAD
+
 				<a href="#mirara">
-=======
-<<<<<<< HEAD
-				<a href="#mirara">
-=======
-				<a href="">
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
+
 
 					Mirar enlinea
 
@@ -446,10 +328,7 @@
 
 	</div>
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
+
 	<div class="enlaces2">
 		<input type="text" name="" placeholder="Buscar pelicula por título">
 
@@ -470,11 +349,6 @@
 	</div>
 
 
-<<<<<<< HEAD
-=======
-=======
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
 	
 </body>
 </html>
--- login.php
+++ login.php
@@ -6,15 +6,9 @@
 	</title>
 </head>
 <meta charset="utf-8">
-<<<<<<< HEAD
+
 <link rel="stylesheet" type="text/css" href="./carpeta/css/login.css?v=0.0.15" media="all">
-=======
-<<<<<<< HEAD
-<link rel="stylesheet" type="text/css" href="./carpeta/css/login.css?v=0.0.15" media="all">
-=======
-<link rel="stylesheet" type="text/css" href="./carpeta/css/login.css?v=0.0.10" media="all">
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
+
 <body>
 
 
@@ -36,15 +30,7 @@
   			<img src="./carpeta/img/contrasena.png">
   			<input type="password" id="email" name="email" placeholder="Contraseña" required="">
   			<br>
-<<<<<<< HEAD
   			<a href="./carpeta/html/registrar.php" target="_blank">¿No tienes una?, ¡Registrate ahora!</a>
-=======
-<<<<<<< HEAD
-  			<a href="./carpeta/html/registrar.php" target="_blank">¿No tienes una?, ¡Registrate ahora!</a>
-=======
-  			<a href="">¿Olvidaste tu contraseña?</a>
->>>>>>> a5f1fbcb0a362cea1bf1331e34805d8b66d2951a
->>>>>>> 4c34de7adca573dd4571202bd4d93bdb2678637b
   			<br>
   			<input type="submit" value="Iniciar sesión" id="submit">
   			
